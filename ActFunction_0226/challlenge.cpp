dfe721a76ee8f6e4eaa9964fb3aa58d1d370b0bb
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-03-02 21:45:05 +0700

    prosedur input data

---
+++ ActFunction_0226/challlenge.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+float Luas, d1, d2;
+
+void ProsedurInputData(){
+    cout << "Masukkan diagonal 1 : ";
+    cin >> d1;
+    cout << "Masukkan diagonal 2 : ";
+}
