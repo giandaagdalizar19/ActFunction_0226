5563d3997211f20653332c022d40db04a5f8d3ee
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-03-02 21:01:54 +0700

    ActFunction_0226

---
+++ ActFunction_0226/p3.cpp
@@ -0,0 +1,13 @@
+//library
+#include <iostream>
+using namespace std;
+//deklarasi variabel global
+int panjang, lebar;
+//implementasi fungsi dan prosedur
+void input() {
+    cout << "Masukkan Panjang : ";
+    cin >> panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+
+}
