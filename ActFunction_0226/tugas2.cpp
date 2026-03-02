e35cd14027100256196770c1e6a9486b94228358
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-03-02 21:36:02 +0700

    fungsi dan prosedur

--- ActFunction_0226/tugas2.cpp
+++ ActFunction_0226/tugas2.cpp
@@ -3,3 +3,16 @@
 using namespace std;
 //deklarsi variabel
 int p, l;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+int luasPersegi(){
+    return p * l;
+}
+void output(){
+    cout << "Hasilnya : " << luasPersegi();
+}
